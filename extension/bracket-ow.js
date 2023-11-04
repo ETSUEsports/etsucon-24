4a924b543bada359d0638488488f3a533df2b3cd
Russell Payne <ryois@ryois.me>
2023-11-03 22:42:29 -0400

    Working OW bracket

--- extension/bracket-ow.js
+++ extension/bracket-ow.js
@@ -2,7 +2,7 @@ module.exports = function (nodecg) {
 	nodecg.log.info("Bracket OW Extension mounted!");
 
     const defaultBracktItem = { team1: { name: 'DEFAULT_TEAM_1', score: 0, color: '#bbbbbb', logo: '' }, team2: { name: 'DEFAULT_TEAM_2', score: 0, color: '#bbbbbb', logo: '' }, winner: 'none' };
-    const defaultBracket = {name: 'Bracket', items: [defaultBracktItem, defaultBracktItem, defaultBracktItem, defaultBracktItem, defaultBracktItem, defaultBracktItem, defaultBracktItem, defaultBracktItem]}
+    const defaultBracket = {name: 'Bracket', winner: {name: "NO_TEAM", score: 0, color: '#bbbbbb', logo: ''}, items: [defaultBracktItem, defaultBracktItem, defaultBracktItem, defaultBracktItem, defaultBracktItem, defaultBracktItem, defaultBracktItem, defaultBracktItem]}
     const bracketOW = nodecg.Replicant('bracketOW', {defaultValue: defaultBracket});
 
     // Listen for changes to the bracket.
